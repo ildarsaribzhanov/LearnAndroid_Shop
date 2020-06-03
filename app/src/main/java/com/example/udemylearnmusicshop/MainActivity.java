fdb90b49edf933cad6c9701617067250678c9753
Ildar Saribzhanov <ildar.r.saribzhanov@gmail.com>
2020-06-03 16:58:15 +0300

    [Add] processing change quantity by buttons

--- app/src/main/java/com/example/udemylearnmusicshop/MainActivity.java
+++ app/src/main/java/com/example/udemylearnmusicshop/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.udemylearnmusicshop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    int quantity = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void incQuantity(View view) {
+        TextView quantityVal = findViewById(R.id.quantityVal);
+
+        if (quantity <= 1) {
+            return;
+        }
+
+        quantity--;
+        quantityVal.setText(Integer.toString(quantity));
+    }
+
+    public void decQuantity(View view) {
+        TextView quantityVal = findViewById(R.id.quantityVal);
+        quantity++;
+        quantityVal.setText(Integer.toString(quantity));
     }
 }
