a72a2150bd46b55ba821435b4587eb561f6dc34d
Ildar Saribzhanov <ildar.r.saribzhanov@gmail.com>
2020-06-04 21:22:56 +0300

    [Add] instruments in spinner

--- app/src/main/java/com/example/udemylearnmusicshop/MainActivity.java
+++ app/src/main/java/com/example/udemylearnmusicshop/MainActivity.java
@@ -4,19 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     int quantity = 1;
+    Spinner spinner;
+    ArrayList instruments;
+    ArrayAdapter instrumentsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinner = findViewById(R.id.spinner);
+        instruments = new ArrayList();
+
+        instruments.add("guitar");
+        instruments.add("sax");
+        instruments.add("violin");
+
+        instrumentsAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, instruments);
+        instrumentsAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(instrumentsAdapter);
     }
 
     public void incQuantity(View view) {
         TextView quantityVal = findViewById(R.id.quantityVal);
+        ArrayList<Integer> age = new ArrayList<Integer>();
+        age.add(1);
 
         if (quantity <= 1) {
             return;
@@ -25,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
         quantity--;
         quantityVal.setText(Integer.toString(quantity));
     }
+
 
     public void decQuantity(View view) {
         TextView quantityVal = findViewById(R.id.quantityVal);
