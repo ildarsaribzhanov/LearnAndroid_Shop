acc5ec22486a51ca0f5b9bcbf63e9bd62ecd8fd0
Ildar Saribzhanov <ildar.r.saribzhanov@gmail.com>
2020-06-05 16:20:43 +0300

    [Add] caclulate selected instrument price

--- app/src/main/java/com/example/udemylearnmusicshop/MainActivity.java
+++ app/src/main/java/com/example/udemylearnmusicshop/MainActivity.java
@@ -4,17 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     int quantity = 1;
     Spinner spinner;
     ArrayList instruments;
     ArrayAdapter instrumentsAdapter;
+
+    Map<String, Double> goodsMap;
+    String instrumentName;
+    double priceItm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         spinner = findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(this);
+
         instruments = new ArrayList();
 
         instruments.add("guitar");
@@ -31,6 +40,11 @@ public class MainActivity extends AppCompatActivity {
         instrumentsAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, instruments);
         instrumentsAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(instrumentsAdapter);
+
+        goodsMap = new HashMap<String, Double>();
+        goodsMap.put("guitar", 500.00);
+        goodsMap.put("sax", 800.00);
+        goodsMap.put("violin", 900.00);
     }
 
     public void incQuantity(View view) {
@@ -51,5 +65,21 @@ public class MainActivity extends AppCompatActivity {
         TextView quantityVal = findViewById(R.id.quantityVal);
         quantity++;
         quantityVal.setText(Integer.toString(quantity));
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        instrumentName = spinner.getSelectedItem().toString();
+        priceItm = goodsMap.get(instrumentName);
+
+        double priceTotal = priceItm * quantity;
+
+        TextView priceView = findViewById(R.id.priceVal);
+        priceView.setText(Double.toString(priceTotal));
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
