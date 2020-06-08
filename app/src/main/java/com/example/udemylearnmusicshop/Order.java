21febab0827579a8428a2567ea4708e4b3223789
Ildar Saribzhanov <ildar.r.saribzhanov@gmail.com>
2020-06-08 21:18:16 +0300

    [Add] implements Parcelable interface for send between activities

--- app/src/main/java/com/example/udemylearnmusicshop/Order.java
+++ app/src/main/java/com/example/udemylearnmusicshop/Order.java
@@ -1,11 +1,14 @@
 package com.example.udemylearnmusicshop;
 
-public class Order {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Order implements Parcelable {
     String userName;
 
     String instrument;
 
-    int amount;
+    Integer amount;
 
     Order() {
     }
@@ -16,6 +19,14 @@ public class Order {
         this.amount = amount;
     }
 
+    public Order(Parcel in) {
+        String[] data = new String[3];
+        in.readStringArray(data);
+        userName = data[0];
+        instrument = data[1];
+        amount = Integer.parseInt(data[2]);
+    }
+
     @Override
     public String toString() {
         return "Order{" +
@@ -24,4 +35,27 @@ public class Order {
                 ", amount=" + amount +
                 '}';
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeStringArray(new String[]{userName, instrument, amount.toString()});
+    }
+
+    public static final Parcelable.Creator<Order> CREATOR = new Parcelable.Creator<Order>() {
+
+        @Override
+        public Order createFromParcel(Parcel source) {
+            return new Order(source);
+        }
+
+        @Override
+        public Order[] newArray(int size) {
+            return new Order[size];
+        }
+    };
 }
