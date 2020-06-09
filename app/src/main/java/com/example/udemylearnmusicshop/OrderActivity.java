60b32a9c47e06406388a8ad08b4f95a38ef9f872
Ildar Saribzhanov <ildar.r.saribzhanov@gmail.com>
2020-06-09 17:09:15 +0300

    [Add] send order by email

--- app/src/main/java/com/example/udemylearnmusicshop/OrderActivity.java
+++ app/src/main/java/com/example/udemylearnmusicshop/OrderActivity.java
@@ -3,11 +3,15 @@ package com.example.udemylearnmusicshop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class OrderActivity extends AppCompatActivity {
+    Order order;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +20,7 @@ public class OrderActivity extends AppCompatActivity {
 
         Intent orderIntent = getIntent();
 
-        Order order = (Order) orderIntent.getParcelableExtra("order");
+        order = (Order) orderIntent.getParcelableExtra("order");
 
         setLayoutData(order);
     }
@@ -33,5 +37,28 @@ public class OrderActivity extends AppCompatActivity {
         quantityView.setText(order.amount.toString());
         priceView.setText(order.cost.toString());
         TotalPriceView.setText(order.getTotalPrice().toString());
+    }
+
+    public void sendOrder(View view) {
+        String message = "User: " + order.userName + "\n" +
+                "Instrument: " + order.instrument + "\n" +
+                "Amount: " + order.amount.toString() + "\n" +
+                "Price One: " + order.cost.toString() + "\n" +
+                "Total price: " + order.getTotalPrice().toString();
+
+        Intent sendEmailIntent = new Intent(Intent.ACTION_SENDTO);
+        sendEmailIntent.setData(Uri.parse("mailto:"));
+        sendEmailIntent.putExtra(Intent.EXTRA_EMAIL, "ildar@saribzhanov.ru");
+        sendEmailIntent.putExtra(Intent.EXTRA_SUBJECT, "App Order");
+        sendEmailIntent.putExtra(Intent.EXTRA_TEXT, message);
+
+        if (sendEmailIntent.resolveActivity(getPackageManager()) == null) {
+            Toast errorToast = Toast.makeText(this, "Not Found send email App", Toast.LENGTH_SHORT);
+
+            errorToast.show();
+            return;
+        }
+
+        startActivity(sendEmailIntent);
     }
 }
