6a541df2ebdb6c524f444a95504490769538ff4a
Ildar Saribzhanov <ildar.r.saribzhanov@gmail.com>
2020-06-10 16:43:46 +0300

    [Fix] to string convert

--- app/src/main/java/com/example/udemylearnmusicshop/OrderItm.java
+++ app/src/main/java/com/example/udemylearnmusicshop/OrderItm.java
@@ -33,8 +33,8 @@ public class OrderItm implements Parcelable {
 
     @Override
     public String toString() {
-        return "Order{" +
-                ", instrument='" + instrument + '\'' +
+        return "OrderItm{" +
+                "instrument='" + instrument + '\'' +
                 ", amount=" + amount +
                 ", cost=" + cost +
                 '}';
